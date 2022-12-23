191742833c7af307cb64c0df705fed50d16d2b86
Feavel <tanuki20966@gmail.com>
2022-12-23 23:47:27 +0800

    cool

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="flex min-h-screen flex-col">
+	<header class="shadow">
+		<nav class="mx-auto flex items-center justify-between px-14 py-5 align-middle text-2xl">
+			<div class="flex w-1/3 justify-start gap-5">
+				<a href="/">Home</a>
+				<a href="/learn">Learn</a>
+				<a href="/community">Community</a>
+			</div>
+
+			<div class="flex w-1/3 justify-end">
+				<div>
+					<a href="/my-account"> My account </a>
+				</div>
+			</div>
+		</nav>
+	</header>
+
+	<slot />
+</main>
