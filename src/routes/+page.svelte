3a6dc755dabe9426ff5a3eaecd3f00a65d3b779f
Feavel <tanuki20966@gmail.com>
2022-12-24 18:25:43 +0800

    cool

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div
+	class="mx-auto flex min-h-screen flex-col content-center justify-center gap-3 text-center font-mono text-3xl"
+>
+	<a href="/en/home">English</a>
+
+	<a href="/ru/home">Русский</a>
+
+	<a href="/zh/home">中文</a>
+</div>
