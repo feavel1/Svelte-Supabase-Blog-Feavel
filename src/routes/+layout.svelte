057349426a058f30c5bdae8fdf90c841cc380949
Feavel <tanuki20966@gmail.com>
2022-12-26 18:20:49 +0800

    cool

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -48,47 +48,29 @@
 	</div>
 </header> -->
 
-<div class="drawer-mobile drawer ">
-	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content flex flex-col items-center justify-center">
-		<!-- Page content here -->
-
-		<label for="my-drawer-2" class="swap-rotate swap btn-circle btn lg:hidden">
-			<!-- this hidden checkbox controls the state -->
-			<input type="checkbox" />
-
-			<!-- hamburger icon -->
-			<svg
-				class="swap-off fill-current"
-				xmlns="http://www.w3.org/2000/svg"
-				width="32"
-				height="32"
-				viewBox="0 0 512 512"
-				><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg
-			>
-
-			<!-- close icon -->
-			<svg
-				class="swap-on fill-current"
-				xmlns="http://www.w3.org/2000/svg"
-				width="32"
-				height="32"
-				viewBox="0 0 512 512"
-				><polygon
-					points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
-				/></svg
-			>
-		</label>
-
-		<div class="mx-auto max-w-4xl overflow-auto py-16 px-4 sm:px-6 lg:px-8 ">
-			<slot />
-		</div>
-	</div>
-	<div class="drawer-side">
-		<label for="my-drawer-2" class="drawer-overlay" />
-		<ul class="menu w-80 bg-accent p-4 text-accent-content">
-			<!-- Sidebar content here -->
-			<li>
+<div class="drawer">
+	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- Navbar -->
+		<div class="navbar  w-full bg-base-300">
+			<div class="flex-none lg:hidden">
+				<label for="my-drawer-3" class="btn-ghost btn-square btn">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="inline-block h-6 w-6 stroke-current"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						/></svg
+					>
+				</label>
+			</div>
+			<div class="mx-2 flex-1 px-2">Feavel</div>
+			<div class="px-2">
 				<label class="swap-rotate swap">
 					<input type="checkbox" data-toggle-theme="mytheme,cmyk" data-act-class="ACTIVECLASS" />
 
@@ -110,7 +92,26 @@
 						/></svg
 					>
 				</label>
-			</li>
+			</div>
+
+			<div class="hidden flex-none lg:block">
+				<ul class="menu menu-horizontal">
+					<!-- Navbar menu content here -->
+					<li><a>Navbar Item 1</a></li>
+					<li><a>Navbar Item 2</a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- Page content here -->
+
+		<div class="drawer-content ">
+			<slot />
+		</div>
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer-3" class="drawer-overlay" />
+		<ul class="menu w-80 bg-base-100 p-4">
+			<!-- Sidebar content here -->
 			<li><a>Sidebar Item 1</a></li>
 			<li><a>Sidebar Item 2</a></li>
 		</ul>
