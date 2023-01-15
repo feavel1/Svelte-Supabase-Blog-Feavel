1857f976bec21c4cd43e3b130224941952725a82
Feavel <tanuki20966@gmail.com>
2023-01-15 12:55:03 +0800

    cool

--- src/routes/community/post/create/+page.svelte
+++ src/routes/community/post/create/+page.svelte
@@ -1,10 +1,34 @@
 <script>
 	import BackTo from '../../../../components/ui/BackTo.svelte';
-	import { createPost } from '$lib/services';
+	import { page } from '$app/stores';
+	import { supabase } from '$lib/supabaseClient';
 
-	function handleCreatePost() {
-		let postTitle = '';
-		let postContent = '';
+	let loading = false;
+
+	let postTitle = '';
+	let postContent = '';
+	let postEmail = $page.data.session.user.email;
+
+	async function handleCreatePost() {
+		try {
+			loading = true;
+
+			const { data, error } = await supabase
+				.from('posts')
+				.insert([{ title: postTitle, content: postContent, email: postEmail }]);
+
+			if (data) {
+			}
+			if (error) {
+				throw error;
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
 	}
 </script>
 
@@ -14,15 +38,16 @@
 	<form class="form-control" on:submit|preventDefault={handleCreatePost}>
 		<div>
 			<label class="label" for="email">帖子标题</label>
-			<input class="input-bordered input-accent input w-full max-w-xs" />
+			<input bind:value={postTitle} class="input-bordered input-accent input w-full max-w-xs" />
 		</div>
 		<div>
 			<label class="label" for="email">帖子内容</label>
 			<textarea
+				bind:value={postContent}
 				class="textarea-accent textarea h-56 w-full"
 				placeholder="# 这里可以输入你想发布的内容,请编写"
 			/>
 		</div>
-		<div class="btn-primary btn mt-4">创建帖子</div>
+		<button disabled={loading} type="button" class="btn">发布帖子</button>
 	</form>
 </div>
