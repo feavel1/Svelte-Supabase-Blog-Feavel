86e7d187c7de786603d88cf520fc26fd0c2c1cc7
Feavel <tanuki20966@gmail.com>
2023-01-30 13:27:21 +0800

    change some

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { env } from '$env/dynamic/public';
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient(
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_ANON_KEY
+);
