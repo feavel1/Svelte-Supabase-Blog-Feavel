3cc6fa84294d474cfa0bfaa7764f5e05d75acf57
Feavel <tanuki20966@gmail.com>
2023-01-30 14:17:35 +0800

    📱Add support for mobile

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-auto';
@@ -8,11 +7,9 @@ import highlighter from './src/lib/components/scripts/codeHighliter.mjs';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
-
 	kit: {
 		adapter: adapter()
 	},
-
 	preprocess: [mdsvex(mdsvexConfig), vitePreprocess()]
 };
 
