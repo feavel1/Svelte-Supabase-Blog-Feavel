78648063b354dd9c2f57c1b313be28efb4a0599b
Feavel <tanuki20966@gmail.com>
2023-01-31 00:33:17 +0800

    check test

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,9 @@ import type { LayoutServerLoad } from './$types';
 import { getServerSession } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutServerLoad = async (event) => {
+	const { url } = event;
 	return {
-		session: await getServerSession(event)
+		session: await getServerSession(event),
+		url: url.pathname
 	};
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -187,7 +187,7 @@
 
 		<!-- Page content here -->
 		<PageTransition key={data.url}>
-			<div class="mt-28 px-6 pb-16 xl:pr-2">
+			<div class="mt-28 px-6 pb-16 xl:px-2">
 				<div class="flex flex-col-reverse justify-center gap-6 md:flex-row ">
 					<slot />
 				</div>
